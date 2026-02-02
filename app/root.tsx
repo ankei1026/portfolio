a9fd8f36209e9bb1ddc62487ffe6bf50f6ff4205
ankei1026 <sabanal.errol12@gmail.com>
2026-02-02 18:52:36 +0800

    feat: landing page + header

--- app/root.tsx
+++ app/root.tsx
@@ -32,6 +32,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
+      <script>AOS.init();</script>
       <body>
         {children}
         <ScrollRestoration />
